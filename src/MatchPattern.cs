7ed206121c01793e06ec7adb8de2c6b1afdc5366
krish Sharma <skrish7373@gmail.com>
2025-09-20 01:00:41 +0530

    Running tests for step 2

---
+++ src/MatchPattern.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+internal class PatternMatcher
+{
+
+    internal bool MatchPattern(string inputLine, string pattern)
+    {
+        Console.WriteLine(inputLine);
+        Console.WriteLine(pattern);
+        if (pattern.Length == 1)
+        {
+             return inputLine.Contains(pattern);
+        }
+        else if(pattern.Length == 2 && pattern == "\\d")
+        {
+            foreach(char ch in inputLine)
+            {
+                if ('0' <= ch - '0' || ch - '0' <= 9)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        else
+        {
+            throw new ArgumentException($"Unhandled pattern: {pattern}");
+        }
+        return false;
+    }
+}
--- src/Program.cs
+++ src/Program.cs
@@ -1,37 +1,31 @@
 using System;
 using System.IO;
 
-static bool MatchPattern(string inputLine, string pattern)
+namespace codecrafters_grep.src
 {
-    if (pattern.Length == 1)
+    internal class Program // Contains the entry point of the application
     {
-        return inputLine.Contains(pattern);
+        static void Main(string[] args)
+        { 
+            if (args[0] != "-E")
+            {
+                Console.WriteLine("Expected first argument to be '-E'");
+                Environment.Exit(2);
+            }
+
+            string pattern = args[1];
+            string inputLine = Console.In.ReadToEnd();
+
+            var matcher = new PatternMatcher();
+
+            if (matcher.MatchPattern(inputLine, pattern))
+            {
+                Environment.Exit(0);
+            }
+            else
+            {
+                Environment.Exit(1);
+            }
+        }
     }
-    else
-    {
-        throw new ArgumentException($"Unhandled pattern: {pattern}");
-    }
-}
-
-if (args[0] != "-E")
-{
-    Console.WriteLine("Expected first argument to be '-E'");
-    Environment.Exit(2);
-}
-
-string pattern = args[1];
-string inputLine = Console.In.ReadToEnd();
-
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-Console.Error.WriteLine("Logs from your program will appear here!");
-
-// Uncomment this block to pass the first stage
-// 
-if (MatchPattern(inputLine, pattern))
-{
-    Environment.Exit(0);
-}
-else
-{
-    Environment.Exit(1);
-}
+  }
