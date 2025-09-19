27915b691051bf0c8fb60ce5cdbf8c6184ef9b4f
krish Sharma <skrish7373@gmail.com>
2025-09-20 01:32:04 +0530

    Pushing changes for the step 4 and then running tests on it

--- src/MatchPattern.cs
+++ src/MatchPattern.cs
@@ -7,9 +7,21 @@ internal class PatternMatcher
     {
         Console.WriteLine(inputLine);
         Console.WriteLine(pattern);
-        if (pattern.Length == 1)
+        if (pattern.Length >= 2 && pattern[0] == '[')
         {
-             return inputLine.Contains(pattern);
+            HashSet<char> charset = new HashSet<char>();
+            for(int i = 1; i < pattern.Length-1; i++)
+            {
+                charset.Add(pattern[i]);
+            }
+            foreach(char ch in inputLine)
+            {
+                if(charset.Contains(ch))
+                {
+                    return true;
+                }
+            }
+            return false;
         }
         else if(pattern == "\\d")
         {
@@ -28,7 +40,7 @@ internal class PatternMatcher
         {
             foreach (char ch in inputLine)
             {
-                Console.WriteLine("Char: " + ch + " Value: " + (int)ch);
+                //Console.WriteLine("Char: " + ch + " Value: " + (int)ch);
                 if (('a' <= ch && ch <= 'z') || ('A' <= ch && ch <= 'Z') || ('0' <= ch && ch <= '9') || ch == '_') 
                     return true;
             }
